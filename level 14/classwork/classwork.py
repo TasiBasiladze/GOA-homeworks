1ed45013350e6bb4b0bfb389cc55aedc98a186a2
TasiBasiladze <tasobasiladze@gmail.com>
2024-07-02 19:16:23 +0400

    commit

---
+++ level 14/classwork/classwork.py
@@ -0,0 +1,24 @@
+#task 1
+list1 = ["book", "notebook", "pen", "pencil", "glue"]
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
+
+#task 2
+list1[1] = "scissors"
+list1[4] = "ruler"
+print(list1)
+
+#task 3
+list2 = [1, 2, 3, 4, 5]
+print(list2[-1])
+print(list2[-2])
+print(list2[-3])
+print(list2[-4])
+print(list2[-5])
+
+list2[-2] = 6
+list2[-5] = 8
+print(list2)
---
+++ level 14/homework/homework.py
@@ -0,0 +1,37 @@
+list1 = [2, 51, 11, 13, 51, 100]
+
+#task 1
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
+print(list1[5])
+
+#task 2
+print(list1[-6])
+print(list1[-5])
+print(list1[-4])
+print(list1[-3])
+print(list1[-2])
+print(list1[-1])
+
+#task 3
+list1[5] = 20
+print(list1)
+
+#task 4
+list1[4] = 67
+print(list1)
+
+#task 5
+print(list1[3:6])
+
+#task 6
+print(list1[0:3])
+
+#task 7
+print(list1[2:4])
+
+#task 8
+print(list1[-5:-1])
