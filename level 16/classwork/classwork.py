04d0715e07efcd12611f90e82084932d83e4a4b8
TasiBasiladze <tasobasiladze@gmail.com>
2024-07-06 18:56:43 +0400

    commit

---
+++ level 16/classwork/classwork.py
@@ -0,0 +1,35 @@
+#task 1
+#      -10 -9 -8 -7 -6 -5 -4 -3 -2  -1
+listn = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#        0  1  2  3  4  5  6  7  8   9
+
+print(listn)
+
+#task 2
+print(listn[0:3])
+print(listn[7:])
+print(listn[3:7])
+
+#task 3
+print(listn[-10:-7])
+print(listn[-3:])
+print(listn[-7:-3])
+
+#task 4
+list1 = [11, 12, 13, 14, 15]
+for i in list1:
+    print(i)
+
+#task 5
+fruit = "apple"
+for i in fruit:
+    print(i)
+
+#task 6
+
+fruits = "Banana,Papaya,Coconut,Fig,Peach"
+print(fruits[:6])
+print(fruits[7:13])
+print(fruits[14:21])
+print(fruits[22:25])
+print(fruits[26:])
---
+++ level 16/homework/homework.py
@@ -0,0 +1,27 @@
+#task 2
+listn = [1, 2, 3, 4, 5]
+for i in listn:
+    print(i * 2)
+
+#task 3
+names = ["taso", "mari", "dachi"]
+for i in names:
+    print("#" + i)
+
+#task 4
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+print(list1 + list2)
+
+#task 5
+vegetables = "potato tomato carrot cabbage pepper cauliflower onion garlic parsley celery"
+print(vegetables[0:6])
+print(vegetables[7:13])
+print(vegetables[14:20])
+print(vegetables[21:28])
+print(vegetables[29:35])
+print(vegetables[36:47])
+print(vegetables[48:53])
+print(vegetables[54:60])
+print(vegetables[61:68])
+print(vegetables[69:])
