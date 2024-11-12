a0d0635b03ef9d28bec558b6706a66d153a098cf
TasiBasiladze <tasobasiladze@gmail.com>
2024-11-12 12:42:17 +0400

    commit

---
+++ level 51/classwork/script.js
@@ -0,0 +1,25 @@
+//task 1
+//JavaScript is used to make some interactions on your website. 
+//fot example, to make some acions when you click button, or not neccesary button. 
+//you can interact anything on your website: images, paragraphs and so on
+
+//task 2
+//event is everything that happens on computer, for example, when you move mouse or click button - it's event
+
+//task 3
+//in JavaScript we declire dynamic variables, we can understand it with the fact that computer understan 
+//itself what is Number and what is String and there's no need to indicate it by 
+//yourself, which you have to when declaration is static
+
+//task 4
+//in JavaScript keywords to declaire variables are var, let and const
+
+// task 5
+//let num = 5;
+//let num = 6;
+//this code will output an error, couse we cant declare same variable twise with 
+//the different values. if we want to change it, we have to write variable's name without keyword (let)
+
+//task 6
+//CamelCase is named for the way the uppercase letters resemble the humps 
+//of a camel. Each word in the phrase starts with an uppercase letter, except the first one.
---
+++ level 51/homework/script.js
@@ -0,0 +1,13 @@
+//task 2
+//Document Object Model is an interface that allows JavaScript to interact with HTML documents, representing them as a tree where each element is a node that can be modified, removed, or added, turning a static page into a dynamic one.
+
+//task 3
+let num1 = 5;
+let num2 = 6;
+console.log(num1 + num2)
+
+//task 4
+//console is an object in JavaScript that provides methods for outputting information to the browser's console. It is used for debugging code and displaying messages such as errors, warnings, calculation results, and other information.
+
+//task 5
+//let is a keyword in JavaScript for declaring variables that can have their values changed but cannot be redeclared in the same scope.
