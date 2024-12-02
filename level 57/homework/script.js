7433e77f12881d28c073fb01e78675508a260382
TasiBasiladze <tasobasiladze@gmail.com>
2024-12-02 15:36:35 +0400

    commit

---
+++ level 57/homework/script.js
@@ -0,0 +1,9 @@
+let confirmAge = confirm("Are you an adult?");
+let adult = "You are Adult";
+let kid = "You are kid";
+
+if (confirmAge) {
+    console.log(adult);
+} else{
+    console.log(kid)
+}
