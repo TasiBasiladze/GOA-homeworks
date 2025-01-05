9b9f3a26153ed05e9683c887bc2eb3fb49418082
TasiBasiladze <tasobasiladze@gmail.com>
2025-01-05 13:32:17 +0400

    commit

---
+++ level 64/homework/homework.py
@@ -0,0 +1,6 @@
+#task 3
+# Instance methods take self and work with instance attributes.
+# Class methods take cls and work at the class level, created with @classmethod.
+# Static methods don’t use self or cls, acting like standalone functions within the class, created with @staticmethod.
+# Differences: Instance methods access object data, class methods access class-level data, and static methods 
+# are independent utility functions.
