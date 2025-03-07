3378ae3febbafe69cc222c7deb4c539a464c12ca
TasiBasiladze <tasobasiladze@gmail.com>
2025-03-07 17:11:11 +0400

    commit

---
+++ level 83/homework/script.js
@@ -0,0 +1,3 @@
+//task 3
+//JavaScript modules allow you to break your code into smaller, reusable pieces by using import and export 
+//(for ES6 modules) or require and module.exports (for CommonJS), making the code more organized, maintainable, and scalable.
